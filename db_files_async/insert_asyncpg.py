3ff060d0be025fa0dbb350eedffb78dfcda0d94d
CaioKeidi <caiokeidi@gmail.com>
2021-04-01 11:42:34 -0300

    fix: some data from postgresql access

--- db_files_async/insert_asyncpg.py
+++ db_files_async/insert_asyncpg.py
@@ -1,6 +1,7 @@
 import asyncio
 import asyncpg
 import datetime
+from db_files_async.password import get_password
 
 async def insert_data(data, conn):
     var1 = await conn.execute("""
@@ -20,11 +21,9 @@ async def insert_data(data, conn):
 
 
 async def inserts(infos):
-    conn = await asyncpg.connect('postgres://postgres:8411mogi@localhost/shoes_scraping')
+    conn = await asyncpg.connect(f'postgres://postgres:{get_password()}@localhost/shoes_scraping')
     for info in infos:
         await insert_data(info, conn)
-    
-    
 
 def main(infos):
     loop = asyncio.get_event_loop()
